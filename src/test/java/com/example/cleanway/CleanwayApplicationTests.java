be7bf8d547fcdb55c5c33bd469ef8daaf2bb224c
jeongeungyeong <jeongeung1124@gmai.com>
2024-04-07 22:12:33 +0900

    4월 7일 기본 셋팅

---
+++ src/test/java/com/example/cleanway/CleanwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
